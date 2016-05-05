0964ca6c606bfb6702a6c72ef150b332d371934b
hds <463141635@qq.com>
2016-05-05 14:43:26 +0800

    添加butterknife

---
+++ app/src/main/java/com/cylan/jiafeigou/view/activity/ShowBigPicActivity.java
@@ -0,0 +1,13 @@
+package com.cylan.jiafeigou.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowBigPicActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_big_pic);
+    }
+}
