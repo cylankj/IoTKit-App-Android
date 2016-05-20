e9df0ee6e8bcca2522e2d80c6a002533deef893e
hds <463141635@qq.com>
2016-05-20 12:49:23 +0800

    添加bugly

---
+++ app/src/main/java/com/cylan/jiafeigou/n/presenter/Task.java
@@ -0,0 +1,7 @@
+package com.cylan.jiafeigou.n.presenter;
+
+/**
+ * Created by hunt on 16-5-20.
+ */
+public interface Task {
+}
---
+++ app/src/main/java/com/cylan/jiafeigou/n/support/BugMonitor.java
@@ -0,0 +1,22 @@
+package com.cylan.jiafeigou.n.support;
+
+import android.content.Context;
+
+import com.cylan.jiafeigou.BuildConfig;
+import com.tencent.bugly.crashreport.CrashReport;
+
+/**
+ * Created by hunt on 16-5-20.
+ */
+public class BugMonitor {
+    /**
+     * 这里的Scene_tag是根据bugly平台上生成的标签的id.
+     */
+    public static final int SCENE_DOORBELLACTIVITY = 15544;
+
+    public static void init(Context context) {
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context);
+        strategy.setAppChannel(BuildConfig.DEBUG ? "DEBUG" : "OFFICIAL_RELEASE");
+        CrashReport.initCrashReport(context, "900026046", false, strategy);
+    }
+}
