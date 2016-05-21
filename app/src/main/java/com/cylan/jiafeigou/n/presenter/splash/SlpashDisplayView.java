e47ec473da9f86620bc9b956c1a85eb2548679b1
hds <463141635@qq.com>
2016-05-21 14:21:46 +0800

    欢迎页面框架

---
+++ app/src/main/java/com/cylan/jiafeigou/n/presenter/splash/SlpashDisplayView.java
@@ -0,0 +1,11 @@
+package com.cylan.jiafeigou.n.presenter.splash;
+
+/**
+ * Created by hunt on 16-5-21.
+ */
+
+public interface SlpashDisplayView {
+
+    int viewCounts();
+
+}
--- app/src/main/java/com/cylan/jiafeigou/n/view/splash/SplashFragment.java
+++ app/src/main/java/com/cylan/jiafeigou/n/view/splash/SplashFragment.java
@@ -3,14 +3,117 @@ package com.cylan.jiafeigou.n.view.splash;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cylan.jiafeigou.R;
+import com.cylan.viewindicator.CirclePageIndicator;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by hunt on 16-5-14.
  */
-public class SplashFragment extends Fragment {
+public class SplashFragment extends Fragment implements SplashView {
+
+    @BindView(R.id.vp_splash_content)
+    ViewPager vpSplashContent;
+    @BindView(R.id.v_indicator)
+    CirclePageIndicator vIndicator;
+    private List<Fragment> fragmentList;
+
+    public static SplashFragment newInstance(Bundle bundle) {
+        SplashFragment fragment = new SplashFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    /**
+     * @param fragments : 提供接口,填充fragment.
+     */
+    public void setSubFragments(List<Fragment> fragments) {
+        this.fragmentList = fragments;
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_splash_layout, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        vpSplashContent.setAdapter(new SplashListAdapter(getChildFragmentManager(), fragmentList));
+        vIndicator.setViewPager(vpSplashContent);
+        vIndicator.setOnPageChangeListener(new SimpleChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+        });
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+
+    private static class SimpleChangeListener implements ViewPager.OnPageChangeListener {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    }
+}
+
+/**
+ * 欢迎页面.
+ */
+class SplashListAdapter extends FragmentPagerAdapter {
+    /**
+     * fragment集合
+     */
+    private List<Fragment> fragmentList;
+
+    public SplashListAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList == null ? 0 : fragmentList.size();
+    }
 }
