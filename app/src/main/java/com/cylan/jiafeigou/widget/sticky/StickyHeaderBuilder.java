7dfed6d4b32afad3d5deed4d5bd7d7dbaceca3a4
hds <463141635@qq.com>
2016-06-02 11:31:02 +0800

    带红点的ImageView添加白色背景'特性'

---
+++ app/src/main/java/com/cylan/jiafeigou/widget/sticky/StickyHeaderBuilder.java
@@ -0,0 +1,8 @@
+package com.cylan.jiafeigou.widget.sticky;
+
+/**
+ * Created by hunt on 16-6-1.
+ */
+
+public class StickyHeaderBuilder {
+}
---
+++ app/src/main/java/com/cylan/jiafeigou/widget/wave/WaveView.java
@@ -0,0 +1,8 @@
+package com.cylan.jiafeigou.widget.wave;
+
+/**
+ * Created by hunt on 16-6-1.
+ */
+
+public class WaveView {
+}
