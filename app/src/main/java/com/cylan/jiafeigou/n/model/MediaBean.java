1d20cb984405baba7636386f19ad323a00752acb
chenguangqiang <457097650@qq.com>
2016-06-08 18:49:20 +0800

    每日精彩部分提交

---
+++ app/src/main/java/com/cylan/jiafeigou/n/model/MediaBean.java
@@ -0,0 +1,12 @@
+package com.cylan.jiafeigou.n.model;
+
+/**
+ * Created by chen on 6/6/16.
+ */
+public class MediaBean {
+    public int idImage = 0;
+    public int mediaType;
+    public String srcFrom;
+    public String curTime;
+}
+
