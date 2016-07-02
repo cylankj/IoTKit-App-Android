5d403a81f1c9b99325779098645d813dfec576c5
hds <463141635@qq.com>
2016-07-02 10:38:00 +0800

    隐藏IME，新方式

--- app/src/main/java/com/cylan/jiafeigou/utils/IMEUtils.java
+++ app/src/main/java/com/cylan/jiafeigou/utils/IMEUtils.java
@@ -1,8 +1,7 @@
 package com.cylan.jiafeigou.utils;
 
 import android.app.Activity;
-import android.content.Context;
-import android.view.inputmethod.InputMethodManager;
+import android.view.WindowManager;
 
 /**
  * Created by cylan-hunt on 16-6-28.
@@ -11,9 +10,10 @@ import android.view.inputmethod.InputMethodManager;
 public class IMEUtils {
     public static void hide(Activity context) {
         if (context != null && context.getCurrentFocus() != null && context.getCurrentFocus().getWindowToken() != null) {
-            InputMethodManager manager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-            manager.hideSoftInputFromWindow(
-                    context.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            context.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+//            InputMethodManager manager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+//            manager.hideSoftInputFromWindow(
+//                    context.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
         }
     }
 }
