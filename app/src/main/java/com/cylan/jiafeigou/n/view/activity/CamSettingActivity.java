900b1651625b58bfceb44ff32eadeca077a0f476
hds <463141635@qq.com>
2016-07-28 14:41:05 +0800

    add camSettingActivity

---
+++ app/src/main/java/com/cylan/jiafeigou/n/view/activity/CamSettingActivity.java
@@ -0,0 +1,17 @@
+package com.cylan.jiafeigou.n.view.activity;
+
+import android.os.Bundle;
+
+import com.cylan.jiafeigou.R;
+import com.cylan.jiafeigou.n.BaseFullScreenFragmentActivity;
+
+public class CamSettingActivity extends BaseFullScreenFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam_setting);
+
+    }
+
+}
