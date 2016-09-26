7059b62b638f8d55d4cff98f3b5e125b8bd717c2
zsl <1045035488@qq.com>
2016-09-26 18:37:59 +0800

    中控界面标题修改

--- app/src/main/java/com/cylan/jiafeigou/n/view/cloud/CloudLiveSettingFragment.java
+++ app/src/main/java/com/cylan/jiafeigou/n/view/cloud/CloudLiveSettingFragment.java
@@ -6,11 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.cylan.jiafeigou.R;
 import com.cylan.jiafeigou.n.mvp.contract.cloud.CloudLiveSettingContract;
+import com.cylan.jiafeigou.utils.ViewUtils;
+import com.cylan.jiafeigou.widget.SettingItemView2;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 作者：zsl
@@ -19,7 +25,18 @@ import butterknife.ButterKnife;
  */
 public class CloudLiveSettingFragment extends Fragment implements CloudLiveSettingContract.View {
 
-    public static CloudLiveSettingFragment newInstance(Bundle bundle){
+    @BindView(R.id.fLayout_top_bar_container)
+    FrameLayout fLayoutTopBarContainer;
+    @BindView(R.id.tv_bell_detail)
+    SettingItemView2 tvBellDetail;
+    @BindView(R.id.tv_bell_detail2)
+    SettingItemView2 tvBellDetail2;
+    @BindView(R.id.tv_setting_clear_)
+    TextView tvSettingClear;
+    @BindView(R.id.tv_setting_unbind)
+    TextView tvSettingUnbind;
+
+    public static CloudLiveSettingFragment newInstance(Bundle bundle) {
         CloudLiveSettingFragment fragment = new CloudLiveSettingFragment();
         fragment.setArguments(bundle);
         return fragment;
@@ -28,13 +45,27 @@ public class CloudLiveSettingFragment extends Fragment implements CloudLiveSetti
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_cloud_live_setting,container,false);
-        ButterKnife.bind(this,view);
+        View view = inflater.inflate(R.layout.fragment_cloud_live_setting, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     public void setPresenter(CloudLiveSettingContract.Presenter presenter) {
 
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        initTopBar();
+    }
+
+
+    private void initTopBar() {
+        ViewUtils.setViewPaddingStatusBar(fLayoutTopBarContainer);
+    }
+
+    @OnClick(R.id.tv_setting_clear_)
+    public void onClick() {
     }
 }
--- app/src/main/java/com/cylan/jiafeigou/n/view/cloud/CloudVideoChatConnetionFragment.java
+++ app/src/main/java/com/cylan/jiafeigou/n/view/cloud/CloudVideoChatConnetionFragment.java
@@ -6,11 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.cylan.jiafeigou.R;
 import com.cylan.jiafeigou.n.mvp.contract.cloud.CloudVideoChatConettionContract;
+import com.cylan.jiafeigou.utils.ToastUtil;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 作者：zsl
@@ -19,22 +25,63 @@ import butterknife.ButterKnife;
  */
 public class CloudVideoChatConnetionFragment extends Fragment implements CloudVideoChatConettionContract.View {
 
-    public static CloudVideoChatConnetionFragment newInstance(Bundle bundle){
+    @BindView(R.id.iv_call_user_image_head)
+    ImageView ivCallUserImageHead;
+    @BindView(R.id.ll_top_layout)
+    LinearLayout llTopLayout;
+    @BindView(R.id.tv_ignore_call)
+    TextView tvIgnoreCall;
+    @BindView(R.id.tv_accept_call)
+    TextView tvAcceptCall;
+
+    private CloudVideoChatConettionOkFragment cloudVideoChatConettionOkFragment;
+
+    public static CloudVideoChatConnetionFragment newInstance(Bundle bundle) {
         CloudVideoChatConnetionFragment fragment = new CloudVideoChatConnetionFragment();
         fragment.setArguments(bundle);
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        cloudVideoChatConettionOkFragment = cloudVideoChatConettionOkFragment.newInstance(new Bundle());
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_cloud_live_videochat_connetion,container,false);
-        ButterKnife.bind(this,view);
+        View view = inflater.inflate(R.layout.fragment_cloud_live_videochat_connetion, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     public void setPresenter(CloudVideoChatConettionContract.Presenter presenter) {
 
+    }
+
+    @OnClick({R.id.iv_call_user_image_head, R.id.tv_ignore_call, R.id.tv_accept_call})
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.iv_call_user_image_head:
+                break;
+            case R.id.tv_ignore_call:
+                ToastUtil.showToast(getContext(),"正在通话中。。。");
+                break;
+            case R.id.tv_accept_call:
+                jump2VideoChatOkFragment();
+                break;
+        }
+    }
+
+    private void jump2VideoChatOkFragment() {
+        getFragmentManager().beginTransaction()
+                .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_right
+                        , R.anim.slide_in_left, R.anim.slide_out_right)
+                .add(android.R.id.content, cloudVideoChatConettionOkFragment, "cloudVideoChatConettionOkFragment")
+                .addToBackStack("cloudVideoChatConettionFragment")
+                .commit();
     }
 }
