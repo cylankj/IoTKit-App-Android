965ba42cb9d1c378a8d832e87ae7d012bd32b395
hds <463141635@qq.com>
2016-10-31 17:33:01 +0800

    status_bar样式定义

--- app/src/main/java/com/cylan/jiafeigou/n/engine/DaemonService.java
+++ app/src/main/java/com/cylan/jiafeigou/n/engine/DaemonService.java
@@ -1,12 +1,15 @@
 package com.cylan.jiafeigou.n.engine;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.widget.RemoteViews;
 
 import com.cylan.jiafeigou.R;
 import com.cylan.jiafeigou.SmartcallActivity;
@@ -36,8 +39,11 @@ public class DaemonService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        //http://www.jianshu.com/p/426d85f34561
         final NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setSmallIcon(R.mipmap.notify_icon);
+        builder.setSmallIcon(R.mipmap.notify_empty);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.remote_view_status_bar);
+        builder.setContent(remoteViews);
         builder.setContentTitle("content title");
         builder.setTicker("ticker");
         builder.setContentText("content text");
@@ -48,9 +54,13 @@ public class DaemonService extends Service {
         notification.flags |= Notification.FLAG_FOREGROUND_SERVICE;
         notification.flags |= Notification.FLAG_NO_CLEAR;
         notification.flags |= Notification.FLAG_ONGOING_EVENT;
+        getNotificationManagerCompat().notify(getApplicationContext().getPackageName().hashCode(), notification);
         startForeground(getApplicationContext().getPackageName().hashCode(), notification);
         return START_STICKY;
     }
 
+    private NotificationManagerCompat getNotificationManagerCompat() {
+        return NotificationManagerCompat.from(getApplicationContext());
+    }
 
 }
