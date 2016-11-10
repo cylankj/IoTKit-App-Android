176ebbda041f8a1267cdea5513c4f4edbdee629d
hds <463141635@qq.com>
2016-11-10 18:44:01 +0800

    rxTest用例

---
+++ app/src/test/java/com/cylan/jiafeigou/support/rxbus/RxTest.java
@@ -0,0 +1,32 @@
+package com.cylan.jiafeigou.support.rxbus;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by cylan-hunt on 16-11-10.
+ */
+
+public class RxTest {
+
+
+//    @Test
+//    public void what() {
+//        Observable.interval(1000, 5000, TimeUnit.MILLISECONDS)
+//                .subscribeOn(Schedulers.newThread())
+//                .observeOn(Schedulers.newThread())
+//                .subscribe(new Action1<Long>() {
+//                    @Override
+//                    public void call(Long aLong) {
+//                        System.out.println("what");
+//                    }
+//                });
+//    }
+
+
+}
