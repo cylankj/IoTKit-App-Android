3a19b2a88dc494bea4904f7ff4ae3f5529d51b0a
hds <463141635@qq.com>
2016-11-29 14:32:48 +0800

    设置设备昵称

--- app/src/main/java/com/cylan/jiafeigou/n/mvp/impl/bind/SetDeviceAliasPresenterImpl.java
+++ app/src/main/java/com/cylan/jiafeigou/n/mvp/impl/bind/SetDeviceAliasPresenterImpl.java
@@ -1,8 +1,18 @@
 package com.cylan.jiafeigou.n.mvp.impl.bind;
 
+import com.cylan.jiafeigou.misc.JfgCmdInsurance;
 import com.cylan.jiafeigou.misc.bind.UdpConstant;
 import com.cylan.jiafeigou.n.mvp.contract.bind.SetDeviceAliasContract;
 import com.cylan.jiafeigou.n.mvp.impl.AbstractPresenter;
+import com.cylan.jiafeigou.support.log.AppLogger;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by cylan-hunt on 16-11-29.
@@ -27,5 +37,28 @@ public class SetDeviceAliasPresenterImpl extends AbstractPresenter<SetDeviceAlia
     @Override
     public void stop() {
 
+    }
+
+    @Override
+    public void setupAlias(String alias) {
+        Observable.just(alias)
+                .subscribeOn(Schedulers.newThread())
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String s) {
+                        AppLogger.i("setup alias: " + portrait);
+                        if (portrait != null)
+                            JfgCmdInsurance.getCmd().setAliasByCid(portrait.cid, s);
+                        return s;
+                    }
+                })
+                .delay(1000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        getView().setupAliasDone();
+                    }
+                });
     }
 }
