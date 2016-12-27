f5c353508ebdb44c84fa305850c8f81c8716d2ea
hds <463141635@qq.com>
2016-12-27 19:00:04 +0800

    给BasePresenter增加更多

--- app/src/main/java/com/cylan/jiafeigou/n/mvp/impl/AbstractPresenter.java
+++ app/src/main/java/com/cylan/jiafeigou/n/mvp/impl/AbstractPresenter.java
@@ -1,17 +1,21 @@
 package com.cylan.jiafeigou.n.mvp.impl;
 
-import com.cylan.jiafeigou.BuildConfig;
+import android.support.annotation.CallSuper;
+
+import com.cylan.jiafeigou.n.mvp.BasePresenter;
 
 import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
 
 /**
  * 一个基本模型的Presenter
  * Created by cylan-hunt on 16-6-30.
  */
-public abstract class AbstractPresenter<T> {
+public abstract class AbstractPresenter<T> implements BasePresenter {
 
     protected final String TAG = this.getClass().getSimpleName();
     protected T mView;//弱引用会被强制释放,我们的view需要我们手动释放,不适合弱引用
+    private CompositeSubscription compositeSubscription;
 
     public AbstractPresenter(T view) {
         mView = view;
@@ -30,10 +34,29 @@ public abstract class AbstractPresenter<T> {
         }
     }
 
-    protected void checkNull() {
-        if (getView() == null && BuildConfig.DEBUG)
-            throw new NullPointerException("view is null");
+    @CallSuper
+    @Override
+    public void start() {
+        if (compositeSubscription == null)
+            compositeSubscription = new CompositeSubscription();
+        if (compositeSubscription.isUnsubscribed()) {
+            compositeSubscription.unsubscribe();
+            compositeSubscription = new CompositeSubscription();
+        }
+        Subscription[] subs = register();
+        if (subs != null) {
+            for (Subscription s : subs)
+                compositeSubscription.add(s);
+        }
     }
 
+    @CallSuper
+    @Override
+    public void stop() {
+        unSubscribe(compositeSubscription);
+    }
 
+    protected Subscription[] register() {
+        return null;
+    }
 }
