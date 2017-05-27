3f9a6254be14b6a278795b30c7aed318af416bb8
hds <463141635@qq.com>
2017-05-27 20:12:57 +0800

    常驻通知栏图标

--- app/src/main/java/com/cylan/jiafeigou/push/WakeupService.java
+++ app/src/main/java/com/cylan/jiafeigou/push/WakeupService.java
@@ -1,13 +1,17 @@
 package com.cylan.jiafeigou.push;
 
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.view.View;
 
 import com.cylan.jiafeigou.NewHomeActivity;
 import com.cylan.jiafeigou.R;
+import com.cylan.jiafeigou.utils.ContextUtils;
 
 public class WakeupService extends Service {
 
@@ -19,13 +23,26 @@ public class WakeupService extends Service {
         return null;
     }
 
+    private int getNotificationSmallIcon() {
+        boolean useWhiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
+        return useWhiteIcon ? R.mipmap.notify_empty : R.mipmap.ic_launcher;
+    }
+
     @Override
     public int onStartCommand(Intent intent, int fla, int startId) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setSmallIcon(R.mipmap.ic_launcher)
+        builder.setSmallIcon(getNotificationSmallIcon())
+                .setLargeIcon(BitmapFactory.decodeResource(ContextUtils.getContext().getResources(),
+                        R.mipmap.ic_launcher))
                 .setContentTitle(getString(R.string.app_name))
                 .setContentIntent(PendingIntent.getActivity(this, 0, new Intent(this, NewHomeActivity.class), 0));
-        startForeground(1, builder.build());
+        final Notification notification = builder.build();
+        int smallIconId = ContextUtils.getContext().getResources().getIdentifier("right_icon", "id", android.R.class.getPackage().getName());
+        if (smallIconId != 0) {
+            notification.contentView.setViewVisibility(smallIconId, View.INVISIBLE);
+//            notification.bigContentView.setViewVisibility(smallIconId, View.INVISIBLE);
+        }
+        startForeground(1, notification);
         return START_STICKY;
     }
 }
