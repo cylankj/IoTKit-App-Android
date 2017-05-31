a0e2c6213ca4d9ce3b2e8bd859f0ca0984f41dab
hds <463141635@qq.com>
2017-05-31 16:31:25 +0800

    调整弹窗UI

--- app/src/main/java/com/cylan/jiafeigou/widget/dialog/PickImageFragment.java
+++ app/src/main/java/com/cylan/jiafeigou/widget/dialog/PickImageFragment.java
@@ -79,5 +79,6 @@ public class PickImageFragment extends BaseDialog {
                 if (takeListener != null) takeListener.onClick(view);
                 break;
         }
+        dismiss();
     }
 }
