c0a6a988e0b96e89d4871c9dad23139f2fc4aff3
hds <463141635@qq.com>
2017-07-18 12:07:42 +0800

    猫眼绑定引导

--- app/src/main/java/com/cylan/jiafeigou/n/view/activity/BindDeviceActivity.java
+++ app/src/main/java/com/cylan/jiafeigou/n/view/activity/BindDeviceActivity.java
@@ -153,8 +153,8 @@ public class BindDeviceActivity extends BaseFullScreenFragmentActivity implement
                 break;
             case R.id.v_to_bind_cat_eye_cam:
                 intent.putExtra(JConstant.KEY_ANIM_GIF, R.raw.eyes_android);
-                intent.putExtra(JConstant.KEY_CONNECT_AP_GIF, R.raw.bind_guide);
-                intent.putExtra(JConstant.KEY_SSID_PREFIX, "DOG-******");
+                intent.putExtra(JConstant.KEY_CONNECT_AP_GIF, R.raw.bind_bell);
+                intent.putExtra(JConstant.KEY_SSID_PREFIX, "BELL-******");
                 intent.putExtra(JConstant.KEY_BIND_DEVICE, getString(R.string.Smart_Door_Viewer));
                 intent.putExtra(JConstant.KEY_ANIM_TITLE, getString(R.string.Tap1_AddDevice_DoorbellTipsTitle));
                 intent.putExtra(JConstant.KEY_ANIM_SUB_TITLE, getString(R.string.Tap1_AddDevice_CameraTips));
