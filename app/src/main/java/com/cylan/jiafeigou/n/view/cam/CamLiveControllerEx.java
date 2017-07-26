742f1ee110918cb83031115b5af147bb71369fdc
hds <463141635@qq.com>
2017-07-26 15:45:46 +0800

    加载录像中,文案

--- app/src/main/java/com/cylan/jiafeigou/n/view/cam/CamLiveControllerEx.java
+++ app/src/main/java/com/cylan/jiafeigou/n/view/cam/CamLiveControllerEx.java
@@ -220,7 +220,7 @@ public class CamLiveControllerEx extends RelativeLayout implements ICamLiveLayer
                     AppLogger.d("点击加载历史视频");
                     layoutE.findViewById(R.id.btn_load_history).setEnabled(false);
                     livePlayState = PLAY_STATE_PREPARE;
-                    setLoadingState(getResources().getString(R.string.LOADING), null);
+                    setLoadingState(getResources().getString(R.string.VIDEO_REFRESHING), null);
                     Subscription subscription = Observable.just("get")
                             .subscribeOn(Schedulers.io())
                             .map(ret -> presenter.fetchHistoryDataList())
@@ -442,14 +442,12 @@ public class CamLiveControllerEx extends RelativeLayout implements ICamLiveLayer
         DpMsgDefine.DPSdStatus status = device.$(204, new DpMsgDefine.DPSdStatus());
         if (!status.hasSdcard || status.err != 0) {
             //隐藏
-//            layoutE.setVisibility(INVISIBLE);
             return;
         }
         //2.手机无网络
         int net = NetUtils.getJfgNetType();
         if (net == 0) {
             //隐藏
-//            layoutE.setVisibility(INVISIBLE);
             return;
         }
         //4.被分享用户不显示
@@ -461,7 +459,6 @@ public class CamLiveControllerEx extends RelativeLayout implements ICamLiveLayer
         //5.设备离线
         if (!JFGRules.isDeviceOnline(device.$(201, new DpMsgDefine.DPNet()))) {
             AppLogger.d("isDeviceOnline false");
-//            layoutE.setVisibility(INVISIBLE);
             return;
         }
         //3.没有历史录像
@@ -1067,6 +1064,14 @@ public class CamLiveControllerEx extends RelativeLayout implements ICamLiveLayer
             post(portHideRunnable);
             setLoadingState(null, null);
         }
+        if (!isLand()) {
+            if (standby.standby) {
+                layoutE.setVisibility(INVISIBLE);
+            } else {
+                layoutE.setVisibility(JFGRules.showSdcard(device) ? VISIBLE : INVISIBLE);
+            }
+        }
+        layoutE.findViewById(R.id.btn_load_history).setEnabled(!standby.standby);
     }
 
     private boolean isStandBy() {
@@ -1320,7 +1325,7 @@ public class CamLiveControllerEx extends RelativeLayout implements ICamLiveLayer
         //获取历史录像ui逻辑
         layoutE.findViewById(R.id.btn_load_history).setEnabled(false);
         livePlayState = PLAY_STATE_PREPARE;
-        setLoadingState(getResources().getString(R.string.LOADING), null);
+        setLoadingState(getResources().getString(R.string.VIDEO_REFRESHING), null);
         Subscription subscription = Observable.just("get")
                 .subscribeOn(Schedulers.io())
                 .flatMap(aBoolean -> RxBus.getCacheInstance().toObservable(RxEvent.HistoryBack.class)
@@ -1427,6 +1432,5 @@ public class CamLiveControllerEx extends RelativeLayout implements ICamLiveLayer
     public void hideHistoryWheel() {
         presenter.getHistoryDataProvider().clean();
         historyWheelHandler.dateUpdate();
-//        layoutE.setVisibility(INVISIBLE);
     }
 }
