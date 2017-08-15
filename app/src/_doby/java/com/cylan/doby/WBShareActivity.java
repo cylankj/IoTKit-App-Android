1154623ac54885c3937ab8ad69ff82c4d38087a2
yanzhendong <923495604@qq.com>
2017-08-15 09:50:58 +0800

    康凯斯门铃调试

--- app/src/_doby/java/com/cylan/doby/WBShareActivity.java
+++ app/src/_doby/java/com/cylan/doby/WBShareActivity.java
@@ -7,5 +7,5 @@ import com.umeng.socialize.media.WBShareCallBackActivity;
 /**
  * Created by wangfei on 15/12/3.
  */
-public class WBShareActivity extends WBShareCallBackActivity {
+public class  WBShareActivity extends WBShareCallBackActivity {
 }
